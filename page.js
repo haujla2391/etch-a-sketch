1f0deaa63431114f94ab40626e4423551b8c53a1
Harman Aujla <harman11524@gmail.com>
2024-09-19 15:42:17 -0400

    update UI features

--- page.js
+++ page.js
@@ -1,6 +1,11 @@
 
 let humanScore = 0;
 let computerScore = 0;
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const scoreDiv = document.querySelector(".score");
+const resultsDiv = document.querySelector(".results");
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -19,10 +24,10 @@ function getComputerChoice(){
     return choice
 }
 
-function getHumanChoice(){
+/* function getHumanChoice(){
     let choice = prompt("Choose rock, paper, or scissors: ")
     return choice.toLowerCase()
-}
+} */
 
 function playRound(humanChoice, computerChoice){
     if (humanChoice === computerChoice) {
@@ -39,10 +44,38 @@ function playRound(humanChoice, computerChoice){
         console.log(`You lose this round! ${computerChoice} beats ${humanChoice}!`);
     }
 
-    console.log(`Scores: Human: ${humanScore}, Computer: ${computerScore}`);
+    scoreDiv.textContent = `Scores: Human: ${humanScore}, Computer: ${computerScore}`;
+
+    checkWinner();
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+function checkWinner(){
+    if(humanScore === 5){
+        resultsDiv.textContent = "Congratulations! You won the game!";
+        disableButtons();
+    } else if(computerScore === 5){
+        resultsDiv.textContent = "Oh no! The computer won the game!";
+        disableButtons();
+    }
+}
 
-playRound(humanChoice,computerChoice);
+function disableButtons(){
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+}
+
+// const humanChoice = getHumanChoice();
+// const computerChoice = getComputerChoice();
+
+rock.addEventListener("click", ()=>{
+    playRound("rock", getComputerChoice());
+})
+
+paper.addEventListener("click", ()=>{
+    playRound("paper", getComputerChoice());
+})
+
+scissors.addEventListener("click", ()=>{
+    playRound("scissors", getComputerChoice());
+})
